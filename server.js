4418b6c95f1cd281eb5ded421c2392a6c3bdd5ae
zeusopdark <ankitnishad400@gmail.com>
2024-03-18 19:44:17 +0530

    Initial commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const corsOptions = {
     origin: "https://assignment-frontend-8373.onrender.com",
     credentials: true,
 }
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
